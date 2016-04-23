e042c9bc449ce902ece076f3e3b21a5884877e66
Rob Vander Sloot <rob@randomvisual.com>
2016-04-22 20:23:31 -0400

    Create Xcode project

---
+++ AutolayoutBasics/AutolayoutBasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutolayoutBasics
+//
+//  Created by Rob Vander Sloot on 4/22/16.
+//  Copyright © 2016 Random Visual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
